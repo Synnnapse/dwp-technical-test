99c29cf392b41b7643f49dd2b8de2688eb64cc8f
Michael Kitchen <michael.kitchen@digital.hmrc.gov.uk>
2020-06-29 09:50:58 +0100

    Handle service exception and further tests

--- app/controllers/LondonResidentsController.scala
+++ app/controllers/LondonResidentsController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc._
 import services.LondonResidentsService
@@ -8,19 +9,31 @@ import services.LondonResidentsService
 @Singleton
 class LondonResidentsController @Inject()(londonResidentsService: LondonResidentsService, val controllerComponents: ControllerComponents) extends BaseController {
 
+  private val logger: Logger = Logger("application." + this.getClass.getCanonicalName)
+
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
   def viewLondonResidents = Action.async { implicit request: Request[AnyContent] =>
     londonResidentsService.getAllLondonResidents.map(
       londonResidents =>
         Ok(views.html.londonResidents(londonResidents))
-    )
+    ).recover {
+      case e: Exception => {
+        logger.warn(s"Something went wrong in service - ${e.toString}")
+        Ok(views.html.technicalDifficulties())
+      }
+    }
   }
 
   def returnLondonResidents = Action.async { implicit request: Request[AnyContent] =>
     londonResidentsService.getAllLondonResidents.map(
       londonResidents =>
         Ok(Json.toJson(londonResidents))
-    )
+    ).recover {
+      case e: Exception => {
+        logger.warn(s"Something went wrong in service - ${e.toString}")
+        Ok(Json.toJson("Sorry we are experiencing technical difficulties. Please try again later."))
+      }
+    }
   }
 }
--- test/controllers/LondonResidentsControllerSpec.scala
+++ test/controllers/LondonResidentsControllerSpec.scala
@@ -5,6 +5,7 @@ import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test.Helpers._
 import play.api.test._
 import services.LondonResidentsService
@@ -14,15 +15,15 @@ import scala.concurrent.ExecutionContext.Implicits._
 
 class LondonResidentsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar {
 
-  "London Residents Controller GET" should {
+  "London Residents Controller viewLondonResidents" should {
 
-    "render the london residents page from a new instance of controller" in {
+    "render the london residents page when service call succeeded" in {
 
       val mockService = mock[LondonResidentsService]
 
       val londonResidents = Seq(Resident(1, "Canary", "Wharf", "email", "ip address", 51.5031, -0.0239),
-      Resident(2, "The", "Shard", "email", "ip address", 51.504501, -0.086500),
-      Resident(3, "Dagenham", "Town", "email", "ip address", 51.55, 0.16667))
+        Resident(2, "The", "Shard", "email", "ip address", 51.504501, -0.086500),
+        Resident(3, "Dagenham", "Town", "email", "ip address", 51.55, 0.16667))
 
       when(mockService.getAllLondonResidents).thenReturn(Future.successful(londonResidents))
 
@@ -32,6 +33,21 @@ class LondonResidentsControllerSpec extends PlaySpec with GuiceOneAppPerTest wit
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("London residents")
+    }
+
+    "render the technical difficulties page when service call failed" in {
+
+      val mockService = mock[LondonResidentsService]
+
+      when(mockService.getAllLondonResidents).thenReturn(Future.failed(new Exception))
+
+      val controller = new LondonResidentsController(mockService, stubControllerComponents())
+      val home = controller.viewLondonResidents().apply(FakeRequest(GET, "/view-london-residents"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) mustNot include ("London residents")
+      contentAsString(home) must include ("technical difficulties")
     }
 
     "render the london residents page from the application" in {
@@ -51,6 +67,41 @@ class LondonResidentsControllerSpec extends PlaySpec with GuiceOneAppPerTest wit
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("London residents")
+    }
+  }
+
+  "London Residents Controller returnLondonResidents" should {
+
+    "return Json for the london residents page when service call succeeded" in {
+
+      val mockService = mock[LondonResidentsService]
+
+      val londonResidents = Seq(Resident(1, "Canary", "Wharf", "email", "ip address", 51.5031, -0.0239),
+        Resident(2, "The", "Shard", "email", "ip address", 51.504501, -0.086500),
+        Resident(3, "Dagenham", "Town", "email", "ip address", 51.55, 0.16667))
+
+      when(mockService.getAllLondonResidents).thenReturn(Future.successful(londonResidents))
+
+      val controller = new LondonResidentsController(mockService, stubControllerComponents())
+      val home = controller.returnLondonResidents().apply(FakeRequest(GET, "/get-london-residents"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("application/json")
+      contentAsJson(home) mustBe Json.toJson(londonResidents)
+    }
+
+    "return error Json when service call failed" in {
+
+      val mockService = mock[LondonResidentsService]
+
+      when(mockService.getAllLondonResidents).thenReturn(Future.failed(new Exception))
+
+      val controller = new LondonResidentsController(mockService, stubControllerComponents())
+      val home = controller.returnLondonResidents().apply(FakeRequest(GET, "/get-london-residents"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("application/json")
+      contentAsJson(home) mustBe Json.toJson("Sorry we are experiencing technical difficulties. Please try again later.")
     }
   }
 }
